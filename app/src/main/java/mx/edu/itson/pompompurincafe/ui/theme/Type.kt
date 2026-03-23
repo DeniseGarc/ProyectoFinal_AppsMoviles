9c2bf369dc9f8dede83edf09e922b2f82b4d4097
DeniseGarc <aliciadenise0623@gmail.com>
2026-03-22 18:08:58 -0700

    fuentes

--- app/src/main/java/mx/edu/itson/pompompurincafe/ui/theme/Type.kt
+++ app/src/main/java/mx/edu/itson/pompompurincafe/ui/theme/Type.kt
@@ -2,18 +2,25 @@ package mx.edu.itson.pompompurincafe.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import mx.edu.itson.pompompurincafe.R
 
+
+val CustomFontFamily = FontFamily(
+    Font(resId = R.font.fredoka_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.fredoka_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.fredoka_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.fredoka_semibold, weight = FontWeight.SemiBold)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = CustomFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
